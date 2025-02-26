ea3f5e9d4e6c87998eabbd7d2cc0c893c459524e
nanana37 <h.nakajima@sslab.ics.keio.ac.jp>
2025-02-26 01:45:58 +0000

    Add support for MOV_MEMb_GPR8 instruction

--- backend/remill/lib/Arch/X86/Arch.cpp
+++ backend/remill/lib/Arch/X86/Arch.cpp
@@ -984,6 +984,7 @@ void SetSemaFuncArgType(Instruction &inst, xed_iform_enum_t iform) {
     case XED_IFORM_MOV_GPRv_GPRv_89:
     case XED_IFORM_LEA_GPRv_AGEN: inst.sema_func_arg_type = SemaFuncArgType::Nothing; break;
     case XED_IFORM_MOV_MEMv_GPRv:
+    case XED_IFORM_MOV_MEMb_GPR8:
     case XED_IFORM_MOV_GPRv_MEMv:
     case XED_IFORM_MOVSXD_GPRv_MEMz:
     case XED_IFORM_PUSH_GPRv_50:
